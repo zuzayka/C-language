25afeddc1eaaff437e07a7d8620fe062cac9a6ca
zuzayka <67962969+zuzayka@users.noreply.github.com>
2020-11-13 18:21:40 +0300

    Update main.c

--- C Programming/Exercises/13/12/main.c
+++ C Programming/Exercises/13/12/main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include<string.h>
 
-void get_extention(const char *file_name, char *extention);
+void get_extension(const char *file_name, char *extension);
 
 int main()
 {
     const char fn[] = {"memo.txt.html"};
     char ext[] = {""};
-    get_extention(fn, ext);
-    printf("Extention = %s\n", ext);
+    get_extension(fn, ext);
+    printf("Extension = %s\n", ext);
     return 0;
 }
 
-void get_extention(const char *file_name, char *extention)
+void get_extension(const char *file_name, char *extension)
 {
     int i = strlen(file_name);
     file_name = file_name + i;
     while(*file_name != '.')
         file_name--;
     file_name++;
-    strcpy(extention, file_name);
+    strcpy(extension, file_name);
 }
